b0bb43ee05ad5d433bf8f9dc05eca80350ed0e57
Sebastian Jerusalem <sebastian.jerusalem@gmx.de>
2022-12-02 17:36:54 +0100

    Initial commit

---
+++ streamlit/pages/1_display_demo.py
@@ -0,0 +1,13 @@
+#importing packages
+
+import streamlit as st
+import streamlit.components.v1 as components
+import numpy as np
+import pandas as pd
+from PIL import Image
+import pickle as pkle
+import os.path
+
+
+
+st.image(uploaded_file, caption="test")
